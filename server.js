8b0a0ccc7e48a210af67ba7383352ac3f9aa21f1
nedmorgan <nmorgan1987@gmail.com>
2019-03-20 14:21:21 -0400

    able to display on development server

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(express.json());
 
 app.use('/api/v1', routes)
 
+app.use(express.static(`${__dirname}/client/build`))
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
