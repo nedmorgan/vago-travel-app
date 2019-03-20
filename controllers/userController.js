7b062d6dce1410033b010442b749f822223d26aa
nedmorgan <nmorgan1987@gmail.com>
2019-03-20 13:55:54 -0400

    updated routes and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,25 @@
 const User = require('../models/User')
 const Country = require('../models/Country')
 
-// Show the users dashboard page
-router.get('/:userId', (req, res) => {
-  res.send('This is the dashboard page for the user and will show the username and countries associated')
-})
+const userController = {
+  index: (req, res) => {
+    res.send('This is the dashboard page for the user and will show the username and countries associated')
+  },
+  create: (req, res) => {
+    res.send('This will add a new country to the database')
+  },
+  updateUser: (req, res) => {
+    res.send('Action to update a user in the database')
+  },
+  updateCountry: (req, res) => {
+    res.send('This will update the name of a country in the database')
+  },
+  deleteUser: (req, res) => {
+    res.send('Action to delete a user from the database')
+  },
+  deleteCountry: (req, res) => {
+    res.send('This will delete a country from the database')
+  },
+}
 
-// This will post a new country to the database
-router.post('/:userId/countries', (req, res) => {
-  res.send('This will add a new country to the database')
-})
-
-// Update a user in the database
-router.put('/:userId', (req, res) => {
-  res.send('Action to update a user in the database')
-})
-
-// Update the name of a country in the database
-router.put('/:userId/countries/:countryId', (req, res) => {
-  res.send('This will update the name of a country in the database')
-})
-
-// Delete a user from the database
-router.delete('/:userId', (req, res) => {
-  res.send('Action to delete a user from the database')
-})
-
-// Delete a country from the database
-router.delete('/:userId/countries/:countryId', (req, res) => {
-  res.send('This will delete a country from the database')
-})
-
-module.exports = router
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,23 @@ router.post('/', appController.create)
 
 // User Controller
 
+// Show the users dashboard page
+router.get('/:userId', userController.index)
+
+// This will post a new country to the database
+router.post('/:userId/countries', userController.create)
+
+// Update a user in the database
+router.put('/:userId', userController.updateUser)
+
+// Update a country in the database
+router.put('/:userId/countries/:countryId', userController.updateCountry)
+
+// Delete a user from the database
+router.delete('/:userId', userController.deleteUser)
+
+// Delete a country from the database
+router.delete('/:userId/countries/:countryId', userController.deleteCountry)
 
 // Country Controller
 
