ad333b08d980d453df0dd5fe6a832ffb8e979460
nedmorgan <nmorgan1987@gmail.com>
2019-03-20 12:30:28 -0400

    created seed file

---
+++ db/seed.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+
+const User = require('../models/User')
+const Country = require('../models/Country')
+
+const italy = new Country({
+  name: 'Italy'
+})
+
+const malaysia = new Country({
+  name: 'Malaysia'
+})
+
+const vietnam = new Country({
+  name: 'Vietname'
+})
+
+const mexico = new Country({
+  name: 'Mexico'
+})
+
+const walter = new User({
+  name: 'Walter',
+  password: 'ilikebones',
+  countries: [italy, malaysia, vietnam, mexico],
+  favorites: [malaysia, mexico]
+})
+
+User.deleteMany({})
+  .then(() => walter.save())
+  .then(() => console.log('Things have been saved to MongoDB'))
+  .then(() => mongoose.connection.close())
