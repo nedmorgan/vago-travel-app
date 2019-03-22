11a58095bdbf97e457391e7090b02d56b73f3a09
nedmorgan <nmorgan1987@gmail.com>
2019-03-22 10:32:34 -0400

    able to obtain user json data

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { UsersContainer } from './styled_components/UsersStyles'
 
 export default class Users extends Component {
+
+  componentDidMount = () => {
+    console.log('Component Mounted')
+    this.getAllUsers()
+  }
+
+  getAllUsers = () => {
+    axios.get('/api/v1/users').then(response => {
+      console.log(response)
+      this.setState({ users: response.data })
+    })
+  }
+
+  state = {
+    users: []
+  }
   render() {
     return (
-      <div>
+      <UsersContainer>
         <h1>Test</h1>
-      </div>
+      </UsersContainer>
     )
   }
 }
---
+++ client/src/components/styled_components/UsersStyles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const UsersContainer = styled.div`
+  h1 {
+    font-family: 'Lato', sans-serif;
+    text-align: center;
+  }
+`
