8c7d05e72bf85be5bc8f0b945dee51ed6ed30ec4
nedmorgan <nmorgan1987@gmail.com>
2019-03-22 12:33:03 -0400

    able to view each country data on the user page

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,26 +1,69 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios'
 import { UserContainer } from './styled_components/UserStyles'
 
 export default class User extends Component {
   state = {
-    user: {},
+    user: {
+      countries: [],
+      favorites: [],
+    },
+    redirectToUsers: false,
   }
 
   componentDidMount = () => {
     axios.get(`/api/v1/users/${this.props.match.params.userId}`).then(response => {
-      console.log(response.data)
-      this.setState({ user: response.data })
-      console.log(this.state.user)
+      this.setState({ user: response.data, redirectToUsers: false })
     })
   }
 
+  deleteIdea = () => {
+    const userId = this.props.match.params.userId
+    axios.delete(`/api/v1/users/${userId}`)
+      .then(res => {
+        this.setState({ user: res.data, redirectToUsers: true })
+      })
+  }
+
   render() {
+    if (this.state.redirectToUsers) {
+      return (<Redirect to='/users' />)
+    }
     return (
-      <div>
-        <h1>Test</h1>
-      </div>
+      <UserContainer>
+        <h1>{this.state.user.name}'s Journey List</h1>
+        <div className="user-info-flex">
+          <h3>Countries</h3>
+          <div className="country-flex">
+            {
+              this.state.user.countries.map(country => {
+                return (
+                  <div>
+                    <Link className="country-button waves-effect waves-light btn-large" key={country._id} to={`/users/${this.state.user._id}/countries/${country._id}`}>
+                      {country.name}
+                    </Link>
+                  </div>
+                )
+              })
+            }
+          </div>
+          <h3>Favorites</h3>
+          <div className="country-flex">
+            {
+              this.state.user.favorites.map(country => {
+                return (
+                  <div>
+                    <Link className="country-button waves-effect waves-light btn-large" key={country._id} to={`/users/${this.state.user._id}/countries/${country._id}`}>
+                      {country.name}
+                    </Link>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </UserContainer>
     )
   }
 }
--- client/src/components/styled_components/UserStyles.jsx
+++ client/src/components/styled_components/UserStyles.jsx
@@ -1,7 +1,19 @@
 import styled from 'styled-components'
 
 export const UserContainer = styled.div`
-  h1 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  h1, h3 {
     font-family: 'Lato', sans-serif;
+  }
+
+  .country-flex {
+    display: flex;
+  }
+
+  .country-button {
+    margin: 1vw;
   }
 `
